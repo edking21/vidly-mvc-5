2c149b69b7398a0a41ba751e3ffaa9e830fbbe6b
Ed King <edking21@gmail.com>
2020-12-09 09:48:18 -0600

    Seed Users

--- Vidly/Migrations/202011231018477_SeedUsers.cs
+++ Vidly/Migrations/202011231018477_SeedUsers.cs
@@ -9,17 +9,11 @@ namespace Vidly.Migrations
         {
             {
                 Sql(@"
-INSERT INTO [dbo].[AspNetUsers] ([Id], [Email], [EmailConfirmed], [PasswordHash], [SecurityStamp], [PhoneNumber], [PhoneNumberConfirmed], [TwoFactorEnabled], [LockoutEndDateUtc], [LockoutEnabled], [AccessFailedCount], [UserName]) 
-VALUES 
-  (N'409516ab-86fb-4b8c-9820-e7da070727f4', N'drgray@gmail.com', 0, N'AGaDUU4OWgCMhjIR6y5ibBASb85rFoLUPxEP3ZZflq/oM7uznoHmiUwZFExLh+c++Q==', N'0f391fc2-f4fb-4a90-ae9d-1b331e8d2a6b', NULL, 0, 0, NULL, 1, 0, N'drgray@gmail.com')
 
 INSERT INTO [dbo].[AspNetUsers] ([Id], [Email], [EmailConfirmed], [PasswordHash], [SecurityStamp], [PhoneNumber], [PhoneNumberConfirmed], [TwoFactorEnabled], [LockoutEndDateUtc], [LockoutEnabled], [AccessFailedCount], [UserName]) 
 VALUES
- (N'8a79e096-052b-4057-9683-7a7443aa305a', N'admin@HL7.com',0,N'AGaDUU4OWgCMhjIR6y5ibBASb85rFoLUPxEP3ZZflq/oM7uznoHmiUwZFExLh+c++Q==', N'0f391fc2-f4fb-4a90-ae9d-1b331e8d2a6b', NULL, 0, 0, NULL, 1, 0, N'admin@HL7.com')
-
-INSERT INTO [dbo].[AspNetUsers] ([Id], [Email], [EmailConfirmed], [PasswordHash], [SecurityStamp], [PhoneNumber], [PhoneNumberConfirmed], [TwoFactorEnabled], [LockoutEndDateUtc], [LockoutEnabled], [AccessFailedCount], [UserName]) 
-VALUES
- (N'b38fafbd-3156-440a-88ab-851b2433fefc', N'guest@HL7.com', 0, N'AGaDUU4OWgCMhjIR6y5ibBASb85rFoLUPxEP3ZZflq/oM7uznoHmiUwZFExLh+c++Q==', N'bb97d846-7e91-4b40-a1cd-94e996ffa47c', NULL, 0, 0, NULL, 1, 0, N'guest@HL7.com')
+ (N'8a79e096-052b-4057-9683-7a7443aa305a', N'admin@vidly.com',0,N'AEl8VTUiWx1HJw4s9BFqDWcxOoVbQ0oSpIKWk5omui3dxjI9lbcr1fgAWZ6nfvepmQ==', 
+N'467b70f2-2e2d-43b5-aa43-df05fb57d231', NULL, 0, 0, NULL, 1, 0, N'admin@vidly.com')
 
 
 INSERT INTO [dbo].[AspNetRoles] ([Id], [Name]) VALUES (N'f5f8ead2-533d-42f1-baa1-7cb018b1ebf5', N'CanManageMovies')
